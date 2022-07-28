6cf26550f51e0bf5b552ef159f962cec4a3be856
Gustavo Silvestre Coutinho <gscoutinho@outlook.com>
2022-07-28 15:51:57 -0300

    deploy back act

--- SpringBoot/miportfolio/src/main/java/com/gscportfolio/miportfolio/MiportfolioApplication.java
+++ SpringBoot/miportfolio/src/main/java/com/gscportfolio/miportfolio/MiportfolioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MiportfolioApplication {
@@ -19,7 +20,7 @@ public class MiportfolioApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfoliogsc.web.app"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
@@ -30,5 +31,4 @@ public class MiportfolioApplication {
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
     }
-
 }
